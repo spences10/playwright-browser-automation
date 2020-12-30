8a66e42b3e981cef86cb00e95d28ca54143b4e6f
spences10 <spences10apps@gmail.com>
2020-12-30 09:43:45 +0000

    update to use all browser types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import * as playwright from "playwright";
 
 async function main() {
-  // launch browser
-  const browser = await playwright.chromium.launch();
-  // create context
-  const context = await browser.newContext();
-  // create page
-  const page = await context.newPage();
+  // use all available browsers
+  const { chromium, firefox, webkit } = playwright;
+  for (const browserType of [chromium, firefox, webkit]) {
+    // launch browser
+    const browser = await browserType.launch();
+    // create context
+    const context = await browser.newContext();
+    // create page
+    const page = await context.newPage();
 
-  // do things with the browser
-  await page.goto(`https://scottspence.com`);
-  await page.screenshot({ path: `output/${playwright.chromium.name()}.png` });
+    // do things with the browser
+    await page.goto(`https://scottspence.com`);
+    await page.screenshot({ path: `output/${playwright.chromium.name()}.png` });
 
-  // close the browser
-  await browser.close();
+    // close the browser
+    await browser.close();
+  }
 }
 
 main();
