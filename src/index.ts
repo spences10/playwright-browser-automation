c37162055e952bcdf84ae91a40ce3190c26ee9d7
spences10 <spences10apps@gmail.com>
2020-12-30 08:59:58 +0000

    simple automation with chromium

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log(`hello world`);
+import * as playwright from "playwright";
+
+async function main() {
+  // launch browser
+  const browser = await playwright.chromium.launch();
+  // create context
+  const context = await browser.newContext();
+  // create page
+  const page = await context.newPage();
+
+  // do things with the browser
+  await page.goto(`https://scottspence.com`);
+  await page.screenshot({ path: `output/${playwright.chromium.name()}.png` });
+
+  // close the browser
+  await browser.close();
+}
+
+main();
